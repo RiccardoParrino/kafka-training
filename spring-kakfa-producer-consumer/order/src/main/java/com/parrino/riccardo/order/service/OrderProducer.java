d30d930487cbca46b6219c35d78ee4d72f11aac6
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-17 11:54:54 +0200

    add producer and consumer order

---
+++ spring-kakfa-producer-consumer/order/src/main/java/com/parrino/riccardo/order/service/OrderProducer.java
@@ -0,0 +1,19 @@
+package com.parrino.riccardo.order.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public OrderProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendOrder(String orderJson) {
+        kafkaTemplate.send("orders", orderJson);
+    }
+
+}
