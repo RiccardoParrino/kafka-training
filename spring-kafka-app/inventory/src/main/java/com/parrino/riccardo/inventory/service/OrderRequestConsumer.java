52184e32717b3d9f65580c118466d00831dc24d8
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-18 10:10:48 +0200

    adding configuration in application properties

--- spring-kafka-app/inventory/src/main/java/com/parrino/riccardo/inventory/service/OrderRequestConsumer.java
+++ spring-kafka-app/inventory/src/main/java/com/parrino/riccardo/inventory/service/OrderRequestConsumer.java
@@ -1,10 +1,16 @@
 package com.parrino.riccardo.inventory.service;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import com.parrino.riccardo.inventory.dto.InventoryRequest;
 
 @Service
 public class OrderRequestConsumer {
     
-    
+    @KafkaListener(topics = "inventory-service", groupId = "inventory-consumer-group")
+    public void consumeInventoryRequest(InventoryRequest inventoryRequest) {
+        System.out.println(inventoryRequest.getOrderId());
+    }
 
 }
--- spring-kafka-app/order/src/main/java/com/parrino/riccardo/order/service/InventoryRequestProducer.java
+++ spring-kafka-app/order/src/main/java/com/parrino/riccardo/order/service/InventoryRequestProducer.java
@@ -1,19 +1,21 @@
 package com.parrino.riccardo.order.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.parrino.riccardo.order.dto.InventoryRequest;
 
 @Service
 public class InventoryRequestProducer {
     
-    /*
-     * Kafka message consist of:
-     * key
-     * value
-     * topic
-     * partition (determined by kafka)
-     * timestamp, headers, etc...
-     */
+    private static final String TOPIC = "inventory-service";
 
-     
+    @Autowired
+    private KafkaTemplate<String, InventoryRequest> kafkaTemplate;
+
+    public void sendInventoryRequest(InventoryRequest inventoryRequest) {
+        kafkaTemplate.send(TOPIC, inventoryRequest);
+    }
 
 }
