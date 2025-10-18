338f23d06e3a550ce6acc163ad402844d5bb7bbb
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-18 12:47:10 +0200

    add InventoryService

---
+++ spring-kafka-json-app/inventory/src/main/java/com/parrino/riccardo/inventory/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.parrino.riccardo.inventory.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.parrino.riccardo.inventory.dto.InventoryRequest;
+
+@Service
+public class InventoryService {
+    
+    @KafkaListener(topics = "inventory-service", groupId = "inventory-group")
+    public void inventoryService (InventoryRequest inventoryRequest) {
+        System.out.println("Received: ");
+        System.out.println("Order: " + inventoryRequest.getOrderId());
+        System.out.println("Product: " + inventoryRequest.getProductId());
+        System.out.println("Quantity: " + inventoryRequest.getQuantity());
+        System.out.println("CollaborationId: " + inventoryRequest.getCollaborationId());
+    }
+
+}
