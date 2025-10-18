484e98fb4cfd811be90c017555b3a648e9f00f9d
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-18 13:28:56 +0200

    adding constructors to InventoryRequest object

--- spring-kafka-json-app/demo/src/main/java/com/parrino/riccardo/dto/InventoryRequest.java
+++ spring-kafka-json-app/demo/src/main/java/com/parrino/riccardo/dto/InventoryRequest.java
@@ -1,12 +1,16 @@
 package com.parrino.riccardo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class InventoryRequest {
     private Long productId;
     private Long orderId;
