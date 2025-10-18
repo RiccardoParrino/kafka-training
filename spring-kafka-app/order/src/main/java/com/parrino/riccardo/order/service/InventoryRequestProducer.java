7ac30e6168151eb355714fb232df844a2884387b
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-18 09:26:07 +0200

    add order request

---
+++ spring-kafka-app/order/src/main/java/com/parrino/riccardo/order/service/InventoryRequestProducer.java
@@ -0,0 +1,19 @@
+package com.parrino.riccardo.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryRequestProducer {
+    
+    /*
+     * Kafka message consist of:
+     * key
+     * value
+     * topic
+     * partition (determined by kafka)
+     * timestamp, headers, etc...
+     */
+
+     
+
+}
---
+++ spring-kafka-app/order/src/main/java/com/parrino/riccardo/order/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.parrino.riccardo.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    
+    public void placeOrder(Long productId, Integer quantity) {
+        
+    }
+
+}
