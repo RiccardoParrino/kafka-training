f84d5feac433ad621f0aed0efbe850106c455b5e
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-18 12:36:53 +0200

    add order producer

---
+++ spring-kafka-json-app/order/src/main/java/com/parrino/riccardo/order/config/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package com.parrino.riccardo.order.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.parrino.riccardo.order.dto.InventoryRequest;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+    
+    @Bean
+    public ProducerFactory<String, InventoryRequest> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, InventoryRequest> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
---
+++ spring-kafka-json-app/order/src/main/java/com/parrino/riccardo/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.parrino.riccardo.order.service;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.parrino.riccardo.order.dto.InventoryRequest;
+
+@Service
+public class OrderService {
+    
+    private static final String TOPIC = "inventory-service";
+
+    @Autowired
+    private KafkaTemplate<String, InventoryRequest> kafkaTemplate;
+
+    public void placeOrder(Long productId, Integer quantity) {
+        String collaborationIdUUID = UUID.randomUUID().toString();
+        InventoryRequest inventoryRequest = InventoryRequest.builder()
+            .productId(productId)
+            .quantity(quantity)
+            .orderId(1l)
+            .collaborationId(collaborationIdUUID)
+        .build();
+
+        kafkaTemplate.send(TOPIC, collaborationIdUUID, inventoryRequest);
+        System.out.println("Inventory request sent for product: " + String.valueOf(productId) + " with quantity: " + quantity);
+    }
+
+}
