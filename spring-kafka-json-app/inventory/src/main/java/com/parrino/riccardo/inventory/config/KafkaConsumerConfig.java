be7607bfdc8b7c2578c2fdb775539a1340d397ee
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-20 11:31:06 +0200

    add inventory consumer response

--- spring-kafka-json-app/inventory/src/main/java/com/parrino/riccardo/inventory/config/KafkaConsumerConfig.java
+++ spring-kafka-json-app/inventory/src/main/java/com/parrino/riccardo/inventory/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,15 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.parrino.riccardo.dto.InventoryRequest;
+import com.parrino.riccardo.dto.InventoryResponse;
 
 @EnableKafka
 @Configuration
@@ -32,10 +39,24 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public ProducerFactory<String, InventoryResponse> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, InventoryRequest> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, InventoryRequest> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, InventoryResponse> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
--- spring-kafka-json-app/inventory/src/main/java/com/parrino/riccardo/inventory/service/InventoryService.java
+++ spring-kafka-json-app/inventory/src/main/java/com/parrino/riccardo/inventory/service/InventoryService.java
@@ -1,20 +1,45 @@
 package com.parrino.riccardo.inventory.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.parrino.riccardo.dto.InventoryRequest;
+import com.parrino.riccardo.dto.InventoryResponse;
 
 @Service
 public class InventoryService {
     
-    @KafkaListener(topics = "inventory-service", groupId = "inventory-group")
+    private static final String TOPIC = "inventory-service";
+    private static final String GROUP = "inventory-group";
+
+    @Autowired
+    private KafkaTemplate<String, InventoryResponse> kafkaTemplate;
+
+    @KafkaListener(topics = TOPIC, groupId = GROUP)
     public void inventoryService (InventoryRequest inventoryRequest) {
         System.out.println("Received: ");
         System.out.println("Order: " + inventoryRequest.getOrderId());
         System.out.println("Product: " + inventoryRequest.getProductId());
         System.out.println("Quantity: " + inventoryRequest.getQuantity());
         System.out.println("CollaborationId: " + inventoryRequest.getCollaborationId());
+
+        // Processing the request
+        InventoryResponse inventoryResponse = InventoryResponse.builder()
+            .orderId(inventoryRequest.getOrderId())
+            .productId(inventoryRequest.getProductId())
+            .available(checkAvailability(inventoryRequest.getProductId(), inventoryRequest.getQuantity()))
+            .collaborationId(inventoryRequest.getCollaborationId())
+        .build();
+
+        kafkaTemplate.send(TOPIC, inventoryRequest.getCollaborationId(), inventoryResponse);
+    }
+
+    private boolean checkAvailability (Long productId, Integer quantity) {
+        if (quantity <= 5)
+            return true;
+        return false;
     }
 
 }
